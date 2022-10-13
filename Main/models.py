f8f0a16ce7edf385b20b4ea55581485f87f16dca
MyEternity <spddarkforce47@gmail.com>
2022-10-13 15:05:00 +0300

    clean base project

--- Main/models.py
+++ Main/models.py
@@ -2,13 +2,15 @@ import uuid
 
 from django.contrib.auth.models import User
 from django.db import models
+
+
 # Create your models here.
 
 
 class ModelCheckStatuses(models.Model):
-    status_id = models.IntegerField(primary_key=True, editable=False, auto_created=True)
-    status_name = models.CharField(max_length=128)
-    finished = models.IntegerField(default=0)
+    status_id = models.IntegerField(primary_key=True, editable=False, auto_created=True, verbose_name='ID статуса')
+    status_name = models.CharField(max_length=128, verbose_name='Статус проверки')
+    finished = models.IntegerField(default=0, verbose_name='Конечный статус')
 
     class Meta:
         db_table = 'check_statuses'
@@ -16,9 +18,9 @@ class ModelCheckStatuses(models.Model):
 
 
 class ModelDocumentStatuses(models.Model):
-    status_id = models.IntegerField(primary_key=True, editable=False, auto_created=True)
-    status_description = models.CharField(max_length=512)
-    status_level = models.IntegerField(default=-1)
+    status_id = models.IntegerField(primary_key=True, editable=False, auto_created=True, verbose_name='ID статуса')
+    status_description = models.CharField(max_length=512, verbose_name='Статус документа')
+    status_level = models.IntegerField(default=-1, verbose_name='Уровень статуса')
 
     class Meta:
         db_table = 'document_statuses'
@@ -26,8 +28,8 @@ class ModelDocumentStatuses(models.Model):
 
 
 class ModelErpStatuses(models.Model):
-    status_id = models.IntegerField(primary_key=True, editable=False, auto_created=True)
-    status_description = models.CharField(max_length=512)
+    status_id = models.IntegerField(primary_key=True, editable=False, auto_created=True, verbose_name='ID статуса')
+    status_description = models.CharField(max_length=512, verbose_name='Статус ERP')
 
     class Meta:
         db_table = 'erp_statuses'
@@ -35,10 +37,10 @@ class ModelErpStatuses(models.Model):
 
 
 class ModelMarkTypes(models.Model):
-    id = models.IntegerField(primary_key=True, editable=False, auto_created=True)
-    description = models.CharField(max_length=512)
-    hs_flag = models.CharField(max_length=64)
-    ts_group_code = models.IntegerField(default=0)
+    id = models.IntegerField(primary_key=True, editable=False, auto_created=True, verbose_name='ID Вида Маркировки')
+    description = models.CharField(max_length=512, verbose_name='Описание вида маркировки')
+    hs_flag = models.CharField(max_length=64, verbose_name='Вид для ВМС')
+    ts_group_code = models.IntegerField(default=0, verbose_name='ID Системы "Честный знак"')
 
     class Meta:
         db_table = 'mark_types'
@@ -46,12 +48,13 @@ class ModelMarkTypes(models.Model):
 
 
 class ModelContractors(models.Model):
-    guid = models.CharField(primary_key=True, editable=False, default=uuid.uuid4, max_length=64)
-    contractor_name = models.CharField(max_length=150)
-    contractor_inn = models.CharField(max_length=32)
-    contractor_count_whitelist = models.IntegerField(default=0)
-    contractor_quality_whitelist = models.IntegerField(default=0)
-    contractor_mrc_minimal = models.IntegerField(default=0)
+    guid = models.CharField(primary_key=True, editable=False, default=uuid.uuid4, max_length=64,
+                            verbose_name='GUID Контрагента')
+    contractor_name = models.CharField(max_length=150, verbose_name='Наименование')
+    contractor_inn = models.CharField(max_length=32, verbose_name='ИНН')
+    contractor_count_whitelist = models.IntegerField(default=0, verbose_name='Белый список по количеству')
+    contractor_quality_whitelist = models.IntegerField(default=0, verbose_name='Белый список по качеству')
+    contractor_mrc_minimal = models.IntegerField(default=0, verbose_name='Контролировать минимальную МРЦ')
 
     class Meta:
         db_table = 'contractors'
@@ -59,9 +62,9 @@ class ModelContractors(models.Model):
 
 
 class ModelContractorContacts(models.Model):
-    contractor_guid = models.ForeignKey(ModelContractors, on_delete=models.CASCADE)
-    email = models.CharField(primary_key=True, unique=True, max_length=128)
-    name = models.CharField(max_length=512)
+    contractor_guid = models.ForeignKey(ModelContractors, on_delete=models.CASCADE, verbose_name='GUID Контакта')
+    email = models.CharField(primary_key=True, unique=True, max_length=128, verbose_name='Электронная почта')
+    name = models.CharField(max_length=512, verbose_name='Наименование')
 
     class Meta:
         db_table = 'contractor_contacts'
@@ -69,19 +72,23 @@ class ModelContractorContacts(models.Model):
 
 
 class ModelDocuments(models.Model):
-    guid = models.CharField(primary_key=True, editable=False, default=uuid.uuid4, max_length=64)
-    contractor_guid = models.ForeignKey(ModelContractors, on_delete=models.CASCADE)
-    order_guid = models.CharField(max_length=64, default=uuid.uuid4)
-    income_guid = models.CharField(max_length=64, default=uuid.uuid4)
-    document_date = models.DateField()
-    document_number = models.CharField(max_length=128)
-    document_name = models.CharField(max_length=512)
-    document_id = models.CharField(max_length=128)
-    check_status_id = models.ForeignKey(ModelCheckStatuses, on_delete=models.DO_NOTHING)
-    whitelisted = models.IntegerField(editable=False)
-    document_status_id = models.ForeignKey(ModelDocumentStatuses, on_delete=models.DO_NOTHING)
-    erp_status_id = models.ForeignKey(ModelErpStatuses, on_delete=models.DO_NOTHING)
-    allowed_grayzone = models.IntegerField(default=0, editable=False)
+    guid = models.CharField(primary_key=True, editable=False, default=uuid.uuid4, max_length=64,
+                            verbose_name='GUID Документа')
+    contractor_guid = models.ForeignKey(ModelContractors, on_delete=models.CASCADE, verbose_name='Контрагент')
+    order_guid = models.CharField(max_length=64, default=uuid.uuid4, verbose_name='Заказ')
+    income_guid = models.CharField(max_length=64, default=uuid.uuid4, verbose_name='Приход')
+    document_date = models.DateField(verbose_name='Дата документа')
+    document_number = models.CharField(max_length=128, verbose_name='Номер документа')
+    document_name = models.CharField(max_length=512, verbose_name='Наименование документа')
+    document_id = models.CharField(max_length=128, verbose_name='Идентификатор ЭДО')
+    check_status_id = models.ForeignKey(ModelCheckStatuses, on_delete=models.DO_NOTHING,
+                                        verbose_name='Статус проверки ЧЗ')
+    whitelisted = models.IntegerField(editable=False, verbose_name='Документ в белом списке')
+    document_status_id = models.ForeignKey(ModelDocumentStatuses, on_delete=models.DO_NOTHING,
+                                           verbose_name='Статус проверки СМ')
+    erp_status_id = models.ForeignKey(ModelErpStatuses, on_delete=models.DO_NOTHING,
+                                      verbose_name='Статус в ERP системе')
+    allowed_gray_zone = models.IntegerField(default=0, editable=False, verbose_name='Разрешена серая зона')
 
     class Meta:
         db_table = 'documents'
