7189b4826fa6dc3c7d49c3c1ac7a5809b17e7b0b
MyEternity <spddarkforce47@gmail.com>
2022-11-09 12:51:27 +0300

    removed unused mixin

--- Marking/mixin.py
+++ Marking/mixin.py
@@ -30,30 +30,6 @@ class UserLoginCheckMixin(View):
             return HttpResponseNotAllowed(request.method)
 
 
-class AjaxableResponseMixin(View):
-    """
-    Mixin to add AJAX support to a form.
-    Must be used with an object-based FormView (e.g. CreateView)
-    """
-
-    def form_invalid(self, form):
-        response = super().form_invalid(form)
-        if self.request.is_ajax():
-            return JsonResponse(form.errors, status=400)
-        else:
-            return response
-
-    def form_valid(self, form):
-        response = super().form_valid(form)
-        if self.request.is_ajax():
-            data = {
-                'pk': self.object.pk,
-            }
-            return JsonResponse(data)
-        else:
-            return response
-
-
 class BaseClassContextMixin(ContextMixin):
     title = ''
 
